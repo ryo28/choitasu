c82b5fc5aaa25fda3b52388cb79339d32c0fc1eb
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-11 17:44:17 +0900

    <body>にクラス名"text-gray-800"を追加し、スタイルを適用しました

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className="text-gray-800">{children}</body>
     </html>
   );
 }

64808b980bd0c092f556cb10888bbf93de664306
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-12 23:42:40 +0900

    ✨ タスク表示を改善し、Todoアイテムの型定義を追加しました。また、色を保持する機能を実装しました

--- src/app/[top]/page.tsx
+++ src/app/[top]/page.tsx
@@ -3,9 +3,20 @@
 import { useState } from "react";
 import { DeletedTask } from "./_components/DeletedTask";
 import { CreateTodos } from "./_components/CreateTodos";
+import { TaskColorChange } from "./_components/TaskColorChange";
+import clsx from "clsx";
+import { Todo } from "./type";
 
+const colors = [
+  "bg-red-100",
+  "bg-green-100",
+  "bg-blue-100",
+  "bg-yellow-100",
+  "bg-purple-100",
+  "bg-white",
+];
 export default function Top() {
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([{ text: "", color: "bg-white" }]);
 
   return (
     <div>
@@ -13,17 +24,34 @@ export default function Top() {
       <div className="p-4">
         <h2 className="text-lg font-bold">タスク</h2>
         <ul>
-          {todos.map((todo, index) => (
-            <div
-              key={index}
-              className="flex justify-between border-b px-4 hover:bg-gray-200 items-center"
-            >
-              <li className="py-2">{todo}</li>
-              <div className="shrink-0">
-                <DeletedTask index={index} setItems={setTodos} items={todos} />
-              </div>
-            </div>
-          ))}
+          {todos.map(
+            (todo, index) =>
+              todo.text !== "" && (
+                <div
+                  key={index}
+                  className={clsx(
+                    "flex justify-between border-b px-4 hover:opacity-75 items-center",
+                    todo.color
+                  )}
+                >
+                  <li className="py-2">{todo.text}</li>
+
+                  <div className="shrink-0 flex gap-8 pl-8">
+                    <TaskColorChange
+                      index={index}
+                      setItems={setTodos}
+                      items={todos}
+                      colors={colors}
+                    />
+                    <DeletedTask
+                      index={index}
+                      setItems={setTodos}
+                      items={todos}
+                    />
+                  </div>
+                </div>
+              )
+          )}
         </ul>
       </div>
     </div>

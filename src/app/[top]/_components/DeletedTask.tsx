3a15acb2b8fa82f4897bf2a01c15028b45d9492f
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-12 23:42:02 +0900

    ✨ DeletedTaskコンポーネントの型定義を修正し、setItemsとitemsをsetTodosとtodosに変更しました

--- src/app/[top]/_components/DeletedTask.tsx
+++ src/app/[top]/_components/DeletedTask.tsx
@@ -1,13 +1,13 @@
-import { IndexProps, ListStateProps } from "../type";
+import { IndexProps, ListStateProps, Todo } from "../type";
 
 export function DeletedTask({
   index,
-  setItems,
-  items,
-}: ListStateProps<string> & IndexProps) {
+  setItems: setTodos,
+  items: todos,
+}: ListStateProps<Todo> & IndexProps) {
   // タスク削除（index指定）
   const handleDelete = (index: number) => {
-    setItems(items.filter((_, i) => i !== index));
+    setTodos(todos.filter((_, i) => i !== index));
     // filterでindexが一致しないものだけを抽出して新しい配列を作成
   };
   return <button onClick={() => handleDelete(index)}>削除</button>;

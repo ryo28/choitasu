6478324e421090c252732d70f1d5de751a001412
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-13 23:19:13 +0900

    ✨ framer-motionを依存関係に追加し、PostCSS設定を更新しました。また、グローバルCSSにherouiのプラグインとソースを追加し、hero.tsを新規作成しました

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,12 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {
+      content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@heroui/**/*.{js,ts,jsx,tsx}",
+      ],
+    },
+  },
 };
 
 export default config;

8b5bdf85069acf9599f9269e68c4bb64818970e7
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-18 00:52:01 +0900

    ✨ DeletedTaskコンポーネントの削除処理をindex指定からid指定に変更し、setSelectedIdsを追加

--- src/app/(top)/_components/DeletedTask.tsx
+++ src/app/(top)/_components/DeletedTask.tsx
@@ -1,14 +1,19 @@
-import { IndexProps, ListStateProps, Todo } from "../type";
+import { ListStateProps, SetState, Todo } from "../type";
 
 export function DeletedTask({
-  index,
+  id: todoId,
   setItems: setTodos,
   items: todos,
-}: ListStateProps<Todo> & IndexProps) {
+  setSelectedIds,
+}: ListStateProps<Todo> & {
+  id: string;
+  setSelectedIds: SetState<string[]>;
+}) {
   // タスク削除（index指定）
-  const handleDelete = (index: number) => {
-    setTodos(todos.filter((_, i) => i !== index));
-    // filterでindexが一致しないものだけを抽出して新しい配列を作成
+  const handleDelete = (id: string) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+    setSelectedIds((prev) => prev.filter((selectedId) => selectedId !== id));
+    // filterでidが一致しないものだけを抽出して新しい配列を作成
   };
-  return <button onClick={() => handleDelete(index)}>削除</button>;
+  return <button onClick={() => handleDelete(todoId)}>削除</button>;
 }

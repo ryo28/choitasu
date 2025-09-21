c656480cb5c375e20f9b8b75d353207303a6ca00
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-22 00:44:14 +0900

    ✨ 削除履歴からデータを削除するコンポーネントを追加

---
+++ src/app/(top)/_components/DeletedHistory.tsx
@@ -0,0 +1,25 @@
+import { Trash2 } from "lucide-react";
+import { SetState, Todo } from "../type";
+
+//削除履歴からデータを削除するコンポーネント
+export function DeletedHistory({
+  id,
+  deletedTodos,
+  setDeletedTodos,
+}: {
+  id: string;
+  deletedTodos: Todo[];
+} & { setDeletedTodos: SetState<Todo[]> }) {
+  // タスク削除（index指定）
+  const handleDeleteHistory = (id: string) => {
+    setDeletedTodos(() =>
+      //idが一致しないものだけを抽出して新しい配列を作成
+      deletedTodos.filter((prev) => prev.id !== id)
+    );
+  };
+  return (
+    <button onClick={() => handleDeleteHistory(id)}>
+      <Trash2 className="text-red-400" />
+    </button>
+  );
+}

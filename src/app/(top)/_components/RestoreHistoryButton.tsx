dc5a4f8d0fd1d7856beec1c174639d3b3ff84acb
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-22 03:43:59 +0900

    ✨ 削除したタスクを復元するボタンコンポーネントを追加

---
+++ src/app/(top)/_components/RestoreHistoryButton.tsx
@@ -0,0 +1,33 @@
+import { Undo2 } from "lucide-react";
+import { ListStateProps, SetState, Todo } from "../type";
+
+//タスク削除ボタンをしつつ、削除したタスクをdeletedTodosに履歴として追加するコンポーネント
+export function RestoreHistoryButton({
+  id: todoId,
+  setItems: setTodos,
+  deletedTodos,
+  setDeletedTodos,
+}: ListStateProps<Todo> & {
+  id: string;
+  deletedTodos: Todo[];
+} & { setDeletedTodos: SetState<Todo[]> }) {
+  // タスク削除（index指定）
+  const handleRestoreTodos = (id: string) => {
+    //選択したidのタスク履歴以外を新し配列のタスク履歴に返す(選択した履歴を削除)
+    setDeletedTodos(deletedTodos.filter((todo) => todo.id !== id));
+    //削除履歴から復元してtodosに追加
+    setTodos((prev) => [
+      ...prev,
+      ...deletedTodos.filter((todo) => todo.id === id),
+    ]);
+  };
+  return (
+    <button
+      onClick={() => handleRestoreTodos(todoId)}
+      title="復元"
+      aria-label="タスクを復元する"
+    >
+      <Undo2 className="text-green-400" />
+    </button>
+  );
+}

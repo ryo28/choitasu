8522914ab577af4ae16bbceab02aef9b8d379eb7
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-22 00:44:23 +0900

    ✨ タスク削除ボタンの機能を改善し、削除したタスクを履歴に追加する処理を整理

--- src/app/(top)/_components/DeletedTask.tsx
+++ src/app/(top)/_components/DeletedTask.tsx
@@ -1,19 +1,31 @@
+import { Delete } from "lucide-react";
 import { ListStateProps, SetState, Todo } from "../type";
 
+//タスク削除ボタンをしつつ、削除したタスクをdeletedTodosに履歴として追加するコンポーネント
 export function DeletedTask({
   id: todoId,
   setItems: setTodos,
   items: todos,
   setSelectedIds,
+  setDeletedTodos,
 }: ListStateProps<Todo> & {
   id: string;
   setSelectedIds: SetState<string[]>;
-}) {
+} & { setDeletedTodos: SetState<Todo[]> }) {
   // タスク削除（index指定）
-  const handleDelete = (id: string) => {
+  const handleDeleteTodos = (id: string) => {
     setTodos(todos.filter((todo) => todo.id !== id));
-    setSelectedIds((prev) => prev.filter((selectedId) => selectedId !== id));
     // filterでidが一致しないものだけを抽出して新しい配列を作成
+    setSelectedIds((prev) => prev.filter((selectedId) => selectedId !== id));
+    //削除したタスクをdeletedTodosに履歴として追加
+    setDeletedTodos((prev) => [
+      ...prev,
+      ...todos.filter((todo) => todo.id === id),
+    ]);
   };
-  return <button onClick={() => handleDelete(todoId)}>削除</button>;
+  return (
+    <button onClick={() => handleDeleteTodos(todoId)}>
+      <Delete className="text-gray-700" />
+    </button>
+  );
 }

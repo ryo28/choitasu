11198466e566f42a84fe7c677dc4e76d13205d0e
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-25 02:08:35 +0900

    ✨ 新しいTodoストアを作成し、状態管理機能を追加

---
+++ src/app/(top)/_store/store.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { Todo } from "../type";
+
+
+// //削除したタスクを保存する配列
+// const [deletedTodos, setDeletedTodos] = useState<Todo[]>([]);
+// //チェックされたタスクをIDで管理してカウントで使うための配列
+// const [selectedIds, setSelectedIds] = useState<string[]>([]);
+// //履歴表示・非表示の状態管理
+// const [showHistory, setShowHistory] = useState(false);
+
+type TodoState = {
+  todos: Todo[];
+  setTodos: (updater: (prev: Todo[]) => Todo[]) => void;
+};
+export const useTodoStore = create<TodoState>((set) => ({
+  todos: [],
+  setTodos: (value: Todo[] | ((prev: Todo[]) => Todo[])) =>
+    set((state) => ({
+      todos:
+        typeof value === "function"
+          ? (value as (prev: Todo[]) => Todo[])(state.todos)
+          : value,
+    })),
+}));

0d98bb71a14d84d40f92b63c843dd0d51dfb116a
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-25 03:05:58 +0900

    ✨ TodoState型の定義を削除し、インポートを整理

--- src/app/(top)/_store/todoStore.ts
+++ src/app/(top)/_store/todoStore.ts
@@ -1,15 +1,12 @@
 import { create } from "zustand";
 import { Todo } from "../type";
+import { TodoState } from "./type";
 
 // //チェックされたタスクをIDで管理してカウントで使うための配列
 // const [selectedIds, setSelectedIds] = useState<string[]>([]);
 // //履歴表示・非表示の状態管理
 // const [showHistory, setShowHistory] = useState(false);
 
-type TodoState = {
-  todos: Todo[];
-  setTodos: (updater: (prev: Todo[]) => Todo[]) => void;
-};
 export const useTodoStore = create<TodoState>((set) => ({
   todos: [],
   setTodos: (value: Todo[] | ((prev: Todo[]) => Todo[])) =>

46536ce69d63558a0d37fc0ffb918d38f9ba1155
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-26 06:43:38 +0900

    🧹 不要なコメントを削除

--- src/app/(top)/_store/todoStore.ts
+++ src/app/(top)/_store/todoStore.ts
@@ -2,11 +2,6 @@ import { create } from "zustand";
 import { Todo } from "../type";
 import { TodoState } from "./type";
 
-// //チェックされたタスクをIDで管理してカウントで使うための配列
-// const [selectedIds, setSelectedIds] = useState<string[]>([]);
-// //履歴表示・非表示の状態管理
-// const [showHistory, setShowHistory] = useState(false);
-
 export const useTodoStore = create<TodoState>((set) => ({
   todos: [],
   setTodos: (value: Todo[] | ((prev: Todo[]) => Todo[])) =>

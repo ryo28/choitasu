3f968065bc6aa32b8a45ee116c914828c91ae65c
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-27 10:06:33 +0900

    🧹 layout.tsxのスタイルを微調整し、パディングを変更

--- src/app/(top)/layout.tsx
+++ src/app/(top)/layout.tsx
@@ -1,7 +1,7 @@
 export default function TopLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-screen bg-gray-50 py-8">
-      <div className="mx-auto max-w-screen-md px-4 border py-8">
+      <div className="mx-auto max-w-screen-md px-2 border py-8">
         {/* メインコンテンツ */}
         <h1 className="text-xl font-bold">TODOリスト</h1>
         {children}

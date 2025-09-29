f27e42f01eefd492e5c1e2a5903b0efa83c19f3b
ryo28 <wakisan1@ezweb.ne.jp>
2025-09-29 22:26:46 +0900

    ✨ Menuコンポーネントを新規作成し、プロファイル編集用のシートを実装

---
+++ src/app/(top)/_components/Menu.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { TextAlignStart } from "lucide-react";
+
+export function SheetDemo() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" className="bg-gray-50">
+          <TextAlignStart />
+          menu
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </SheetDescription>
+        </SheetHeader>
+        <footer className="grid flex-1 auto-rows-min gap-6 px-4">
+          <div className="grid gap-3">このアプリについて</div>
+          <div className="grid gap-3">プライバシーポリシー</div>
+          <div className="grid gap-3">利用規約</div>
+          <div className="grid gap-3">Contact</div>
+          <div className="grid gap-3">
+            © 2025~{new Date().getFullYear()} Wakisan. All rights reserved.
+          </div>
+        </footer>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}

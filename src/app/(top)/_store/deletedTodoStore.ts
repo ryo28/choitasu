0fd52c828dd63c11958b87517c595306678f7e0a
ryo28 <wakisan1@ezweb.ne.jp>
2025-10-02 15:57:02 +0900

    ✨ deletedTodoStore、selectedIdStore、todoStoreのpersistミドルウェアを追加し、ストレージの永続化を実装

--- src/app/(top)/_store/deletedTodoStore.ts
+++ src/app/(top)/_store/deletedTodoStore.ts
@@ -1,14 +1,20 @@
 import { create } from "zustand";
 import { TodoState } from "./type";
 import { Todo } from "../type";
+import { persist } from "zustand/middleware";
 
-export const useDeletedTodoStore = create<TodoState>((set) => ({
-  todos: [],
-  setTodos: (value: Todo[] | ((prev: Todo[]) => Todo[])) =>
-    set((state) => ({
-      todos:
-        typeof value === "function"
-          ? (value as (prev: Todo[]) => Todo[])(state.todos)
-          : value,
-    })),
-}));
+export const useDeletedTodoStore = create<TodoState>()(
+  persist(
+    (set) => ({
+      todos: [],
+      setTodos: (value: Todo[] | ((prev: Todo[]) => Todo[])) =>
+        set((state) => ({
+          todos:
+            typeof value === "function"
+              ? (value as (prev: Todo[]) => Todo[])(state.todos)
+              : value,
+        })),
+    }),
+    { name: "deleted-todo-storage" }
+  )
+);
--- src/app/(top)/_store/todoStore.ts
+++ src/app/(top)/_store/todoStore.ts
@@ -1,16 +1,24 @@
 import { create } from "zustand";
 import { Todo } from "../type";
 import { TodoState } from "./type";
-import { todoTestItems } from "./todoTestItem";
+import { todoTestItems } from "./todoTestItem"; //仮のデータ
+import { persist } from "zustand/middleware";
 
 //空配列で初期化[]
-export const useTodoStore = create<TodoState>((set) => ({
-  todos: todoTestItems,
-  setTodos: (value: Todo[] | ((prev: Todo[]) => Todo[])) =>
-    set((state) => ({
-      todos:
-        typeof value === "function"
-          ? (value as (prev: Todo[]) => Todo[])(state.todos)
-          : value,
-    })),
-}));
+export const useTodoStore = create<TodoState>()(
+  persist(
+    (set) => ({
+      todos: [],
+      setTodos: (value: Todo[] | ((prev: Todo[]) => Todo[])) =>
+        set((state) => ({
+          todos:
+            typeof value === "function"
+              ? (value as (prev: Todo[]) => Todo[])(state.todos)
+              : value,
+        })),
+    }),
+    {
+      name: "todo-storage", // localStorage のキー名
+    }
+  )
+);

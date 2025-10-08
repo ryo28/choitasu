87328179b606949adcf029e2b4a7d5b5fb423cc4
ryo28 <wakisan1@ezweb.ne.jp>
2025-10-09 01:02:53 +0900

    🐛  deletedTodoStore.tsのcleanOldTodosメソッドで日付の解析にformatISOからparseに変更し、エラーを解消

--- src/app/(top)/_store/deletedTodoStore.ts
+++ src/app/(top)/_store/deletedTodoStore.ts
@@ -1,32 +1,38 @@
+import { differenceInDays, parse } from "date-fns";
+import { ja } from "date-fns/locale";
 import { create } from "zustand";
-import { TodoState } from "./type";
-import { DeletedTodo } from "../type";
 import { persist } from "zustand/middleware";
-import { differenceInDays, formatISO } from "date-fns";
+import type { DeletedTodo } from "../type";
+import type { TodoState } from "./type";
 
 export const useDeletedTodoStore = create<TodoState<DeletedTodo>>()(
-  persist(
-    (set, get) => ({
-      todos: [],
-      setTodos: (
-        value: DeletedTodo[] | ((prev: DeletedTodo[]) => DeletedTodo[])
-      ) =>
-        set((state) => ({
-          todos:
-            typeof value === "function"
-              ? (value as (prev: DeletedTodo[]) => DeletedTodo[])(state.todos)
-              : value,
-        })),
-      // 30日以上前の削除済みタスクを自動で削除するメソッド
-      cleanOldTodos: () => {
-        const now = new Date();
-        const validTodos = get().todos.filter((todo) => {
-          const todoDate = formatISO(new Date(todo.date)); // 文字列をDate型に変換 differenceInDaysの引数はDate型である必要がある
-          return differenceInDays(now, todoDate) < 30;
-        });
-        set({ todos: validTodos });
-      },
-    }),
-    { name: "deleted-todo-storage" }
-  )
+	persist(
+		(set, get) => ({
+			todos: [],
+			setTodos: (
+				value: DeletedTodo[] | ((prev: DeletedTodo[]) => DeletedTodo[]),
+			) =>
+				set((state) => ({
+					todos:
+						typeof value === "function"
+							? (value as (prev: DeletedTodo[]) => DeletedTodo[])(state.todos)
+							: value,
+				})),
+			// 30日以上前の削除済みタスクを自動で削除するメソッド
+			cleanOldTodos: () => {
+				const now = new Date();
+				const validTodos = get().todos.filter((todo) => {
+					const todoDate = parse(
+						todo.date,
+						"yyyy MM月d日 (E) HH:mm",
+						new Date(),
+						{ locale: ja },
+					); // 文字列をDate型に変換 differenceInDaysの引数はDate型である必要がある
+					return differenceInDays(now, todoDate) < 30;
+				});
+				set({ todos: validTodos });
+			},
+		}),
+		{ name: "deleted-todo-storage" },
+	),
 );

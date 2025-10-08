1a67d9af1ef84c2d8c8e90f9cacb10e5015c3c1d
ryo28 <wakisan1@ezweb.ne.jp>
2025-10-09 01:03:30 +0900

    ♻️ コードのインデントを統一し、可読性を向上

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,12 @@
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {
-      content: [
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./node_modules/@heroui/**/*.{js,ts,jsx,tsx}",
-      ],
-    },
-  },
+	plugins: {
+		"@tailwindcss/postcss": {
+			content: [
+				"./src/**/*.{js,ts,jsx,tsx}",
+				"./node_modules/@heroui/**/*.{js,ts,jsx,tsx}",
+			],
+		},
+	},
 };
 
 export default config;
